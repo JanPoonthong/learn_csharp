6e3f9fac6c6edc54a311bbc8df9633b3c5c43780
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-11 11:50:05 +0700

    chore: remove obj and bin folders

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, David!");
+Console.WriteLine("Hello, Jan!");
+
+string firstFriend = "Jonas";
+string secondFriend = "Emilie";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+string greeting = "      Hello World     ";
+Console.WriteLine($"{greeting}");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
