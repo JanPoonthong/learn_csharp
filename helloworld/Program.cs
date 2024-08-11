a6d01f80f14b40870c6e1bb4842fbf504c8d8435
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-11 11:56:14 +0700

    feat: replace string

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -4,16 +4,6 @@ Console.WriteLine("Hello, Jan!");
 string firstFriend = "Jonas";
 string secondFriend = "Emilie";
 
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+string friend = $"My friends are {firstFriend} and {secondFriend}";
 
-string greeting = "      Hello World     ";
-Console.WriteLine($"{greeting}");
-
-string trimmedGreeting = greeting.TrimStart();
-Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
-
-trimmedGreeting = greeting.TrimEnd();
-Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
-
-trimmedGreeting = greeting.Trim();
-Console.WriteLine($"{trimmedGreeting} Jan Poonthong");
+Console.WriteLine(friend.Replace("Jonas", "Nam"));
